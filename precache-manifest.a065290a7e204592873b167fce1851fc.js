f017dd7a0cc4819c72701f84d3472338ae581572
KenAdams777 <andrey.kalyshenko@gmail.com>
2022-01-15 19:54:54 +0200

    Updates

---
+++ precache-manifest.a065290a7e204592873b167fce1851fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e802cfd044d1a814afe505cdaddc60",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "ff89741ed43f7470bd03",
+    "url": "/Tic-Tac-Toe/static/css/main.dd97c41b.chunk.css"
+  },
+  {
+    "revision": "a006dd1e1523f1d88e9c",
+    "url": "/Tic-Tac-Toe/static/js/2.2d87014d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Tic-Tac-Toe/static/js/2.2d87014d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff89741ed43f7470bd03",
+    "url": "/Tic-Tac-Toe/static/js/main.dca90985.chunk.js"
+  },
+  {
+    "revision": "2a864d222fa5edbea455",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.954e996b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc97271a594115bdff17c0051bf26fc9.js"
+  "/Tic-Tac-Toe/precache-manifest.a065290a7e204592873b167fce1851fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tic-Tac-Toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
